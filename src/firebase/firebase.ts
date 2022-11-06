da7e91c96e3d78ba30e9b06488d14dea92a67c3d
Surendran Jagadeesh <surendranjagadeesh@gmail.com>
2022-11-06 22:17:52 +0530

    Firebase config

--- src/firebase/firebase.ts
+++ src/firebase/firebase.ts
@@ -5,22 +5,13 @@ import { getAuth } from "firebase/auth";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
-// const firebaseConfig = {
-//     apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-//     authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-//     projectId: process.env.NEXT_PUBLIC_PROJ_ID,
-//     storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-//     messagingSenderId: process.env.NEXT_PUBLIC_MSGING_SENDER_ID,
-//     appId: process.env.NEXT_PUBLIC_APP_ID,
-// };
-
 const firebaseConfig = {
-    apiKey: "AIzaSyBDn69Bz4h_wkinh6qjNf-gOzi2mlt0Y_s",
-    authDomain: "primax-685eb.firebaseapp.com",
-    projectId: "primax-685eb",
-    storageBucket: "primax-685eb.appspot.com",
-    messagingSenderId: "169060467940",
-    appId: "1:169060467940:web:5630460447f6a28f9298f1",
+    apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    authDomain: process.env.NEXT_PUBLIC_AUTH_DOMAIN,
+    projectId: process.env.NEXT_PUBLIC_PROJ_ID,
+    storageBucket: process.env.NEXT_PUBLIC_STORAGE_BUCKET,
+    messagingSenderId: process.env.NEXT_PUBLIC_MSGING_SENDER_ID,
+    appId: process.env.NEXT_PUBLIC_APP_ID,
 };
 
 // Initialize Firebase
